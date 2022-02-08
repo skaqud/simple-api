17b6c145de97d163de490e4434e5929098391690
skaqud <skaqud@paran.com>
2022-02-08 09:34:26 +0000

    initial commit

---
+++ src/main/java/com/kt/icis/simpleapi/SimpleController.java
@@ -0,0 +1,20 @@
+package com.kt.icis.simpleapi;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/v1")
+public class SimpleController {
+
+    @GetMapping("/simple")
+    public String getSimple() {
+        return "Simple-API Service";
+    }
+    
+}
---
+++ src/test/java/com/kt/icis/simpleapi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.icis.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
