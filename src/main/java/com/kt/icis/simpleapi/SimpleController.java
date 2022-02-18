970a488726c57d65234de3dedd76be4fea4d1c20
skaqud <skaqud@paran.com>
2022-02-18 16:48:30 +0900

    update version RB0.0.10

--- src/main/java/com/kt/icis/simpleapi/SimpleController.java
+++ src/main/java/com/kt/icis/simpleapi/SimpleController.java
@@ -14,7 +14,7 @@ public class SimpleController {
 
     @GetMapping("/simple")
     public String getSimple() {
-        return "Simple-API Service99";
+        return "Simple-API Service10";
     }
     
 }
